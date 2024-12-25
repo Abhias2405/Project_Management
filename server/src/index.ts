e3f439226f02f3ffc7c4c434df922e0ef9d3d512
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-12-25 14:14:44 +0530

    add authorization

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
 /* ROUTE IMPORTS */
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
